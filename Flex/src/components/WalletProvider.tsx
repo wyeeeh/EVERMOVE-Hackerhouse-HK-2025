6242753b8a0643dda97dc7ce921f8fe6cba3345c
Ye <sheohun@gmail.com>
2025-04-02 14:13:24 +0800

    update folder

---
+++ Flex/src/components/WalletProvider.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+// Internal components
+import { useToast } from "@/components/ui/use-toast";
+// Internal constants
+import { APTOS_API_KEY, NETWORK } from "@/constants";
+import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
+import type { PropsWithChildren } from "react";
+
+export function WalletProvider({ children }: PropsWithChildren) {
+  const { toast } = useToast();
+
+  return (
+    <AptosWalletAdapterProvider
+      autoConnect={true}
+      dappConfig={{ network: NETWORK, aptosApiKey: APTOS_API_KEY }}
+      optInWallets={["Continue with Google","Petra","Nightly","Pontem Wallet", "Mizu Wallet"]}
+      onError={(error) => {
+        toast({
+          variant: "destructive",
+          title: "Error",
+          description: error || "Unknown wallet error",
+        });
+      }}
+    >
+      {children}
+    </AptosWalletAdapterProvider>
+  );
+}
